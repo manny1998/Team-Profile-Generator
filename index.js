5b7dd9af71f8d843feca8522390a49dc4851d97a
Manny <manroven-singh@hotmail.com>
2022-05-19 13:02:04 +0930

    adding files e.g. git ignore, test, etc

---
+++ index.js
@@ -0,0 +1,6 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const Employee = require("./lib/Employee");
